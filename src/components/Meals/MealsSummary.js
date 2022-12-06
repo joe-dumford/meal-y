4f6004c1b25cdfc8a0ffb3d0ed370ee5e41a8ecf
Joe D <dumford.joe@gmail.com>
2022-12-05 20:12:20 -0500

    Created meals list, summary and dummy data. Light  styling

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,11 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+    return <section className={classes.summary}>
+        <h2>Delicious Food, Delievered to you</h2>
+        <p>Choose Stuff</p>
+        <p>Choose MORE Stuff</p>
+    </section>
+}
+
+export default MealsSummary;
