e774228ffc7c44e36134cdbd24af41cfedfa281a
Joe D <dumford.joe@gmail.com>
2022-12-14 19:22:45 -0500

    Started Cart Modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import classes from './Cart.module.css';
+import Modal from '../UI/Modal';
 
 const Cart = props => {
     const cartItems = <ul className={'cart-items'}>{[
@@ -12,7 +13,7 @@ const Cart = props => {
     </ul>;
 
     return (
-        <div>
+        <Modal>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
@@ -22,7 +23,7 @@ const Cart = props => {
                 <button className={classes['button--alt']}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     )
 }
 
