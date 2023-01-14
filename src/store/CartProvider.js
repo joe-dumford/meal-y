35febd0e3129d9345bf283cfd7ccd1e876fb6895
Joe D <dumford.joe@gmail.com>
2023-01-14 17:49:07 -0500

    implementing useReducer and added more logic to cart state context. More needed, done for now.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,39 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
-const CartProvider = props => {
-    const addItemToCartHandler = (item) => {}
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
 
-    const removeItemFromCartHandler = (id) => {}
+const cartReducer = (state, action) => {
+    if (action.identifier === 'ADD_ITEM') {
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    } else if (action.identifier === 'REMOVE_ITEM') {
+
+    }
+    return defaultCartState;
+};
+
+const CartProvider = props => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({identifier: 'ADD_ITEM', item: item});
+    }
+
+    const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({identifier: 'REMOVE_ITEM', id: id});
+    }
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
     }
